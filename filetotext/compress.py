fcf638b0a3210a8940ae35e056b0fb584b81d4bd
irfnykcr <irfnykcr@protonmail.com>
2024-08-31 01:49:37 +0300

    added compress.py

--- filetotext/compress.py
+++ filetotext/compress.py
@@ -1,5 +1,10 @@
 from deflate import zlib_compress
-
+from os import listdir
 def compress(dirpath: str, level: int = 6):
-    
-    return zlib_compress(filepath, level)
+	for file in listdir(dirpath):
+		with open(file, "rb") as f:
+			data = f.read()
+		r = zlib_compress(data, level)
+		with open(file, "wb") as f:
+			f.write(str(len(data)).encode("utf-8") + " " + r)
+			return True
